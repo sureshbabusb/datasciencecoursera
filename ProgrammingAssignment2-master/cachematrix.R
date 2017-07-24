f81c33389d5395f0217838e452dfdd29c592a1ea
Sureshbabu Sengodan <sureshbabusb@gmail.com>
2017-07-23 21:03:53 -0400

    inverse code added

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,51 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+  i <- NULL
+  
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  
+  ##setinverse <- function(inv) i <<- inv
+  
+  ##getinverse <- function() i
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
+}
+
+
+getinverse <- function() {
+  return(i)
+}
+
+setinverse <- function(inv) {
+  i <<- inv
+}
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  
+   i <<- getinverse()
+  
+  if (!is.null(i)) {
+    return(i)
+  }
+  
+  data <- solve(x)
+  setinverse(data)
+  i
+  
+}
